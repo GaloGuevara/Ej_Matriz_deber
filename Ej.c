93436f65c0d89454f22e2b5293deec5b7d0ba412
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-11-21 15:04:51 -0500

    Inclusión de bibliotecas y creación de la estructura main

---
+++ Ej.c
@@ -0,0 +1,16 @@
+/*Construya un programa en lenguaje C que:
+
+    Cree una matriz cuyas dimensiones sean ingresadas por el usuario. La matriz puede tener cualquier dimensión.
+    Inicialice toda la matriz en ceros.
+    Imprima la matriz con la diagonal principal con valores en 1.
+
+Entregue la URL a su repositorio de Github.*/
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
